1515b5fbd5c7f2f48fe0792d06911a1e8d4db49f
Parasrah <git@parasrah.com>
2021-01-21 22:11:32 -0700

    remove reselect

--- src/selectors.js
+++ src/selectors.js
@@ -1,5 +1,2 @@
-import { createSelector } from 'reselect'
-
-export const players = createSelector((users) =>
-  users.filter((x) => x.active).map((x) => x.data._id),
-)
+export const players = (users) =>
+  users.filter((x) => x.active).map((x) => x.data._id)
