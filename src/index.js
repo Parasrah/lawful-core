06f886374f2dfe16d5873045fb9fe38887ca971c
Parasrah <git@parasrah.com>
2021-01-21 22:42:09 -0700

    init -> ready hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import createCore from './store'
 import * as select from './selectors'
 
-Hooks.once('init', () => {
+Hooks.once('init', () => {})
+
+Hooks.once('ready', () => {
   game.lawful = createCore(game)
 })
-
-Hooks.once('ready', () => {})
 
 Hooks.on('renderPlayerList', (_, __, { users }) => {
   const { lawful } = game
