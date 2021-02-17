9307773722190b64d47d1daad5c9117332874cec
Parasrah <git@parasrah.com>
2021-02-16 20:45:59 -0700

    use getParticipants everywhere #7 #patch

--- src/logic/merchant.ts
+++ src/logic/merchant.ts
@@ -1,61 +1,9 @@
 import { PurchaseAction, SellAction } from '../actions'
 import MultiTransaction from '../apps/multiTransaction'
+import getParticipants from '../util/getParticipants'
 import notify from '../util/notify'
 import * as currency from './currency'
-import * as items from './items'
-
-type Actor5e = game.dnd5e.entities.Actor5e
-type Item5e = game.dnd5e.entities.Item5e
-
-async function transferCurrency(
-  fromActor: Actor5e,
-  toActor: Actor5e,
-  amount: Currency,
-) {
-  const from = fromActor.data.data.currency
-  const to = toActor.data.data.currency
-  const { from: freshFrom, to: freshTo } = currency.transfer({
-    amount,
-    from,
-    to,
-  })
-  await currency.updateActor(fromActor, freshFrom)
-  await currency.updateActor(toActor, freshTo)
-}
-
-async function addItem(actor: Actor5e, item: Item5e, count: number = 1) {
-  const ownedItem = actor.items.find((i) => items.compare(i, item))
-  if (ownedItem) {
-    ownedItem.update({
-      data: { quantity: ownedItem.data.data.quantity + count },
-    })
-  } else {
-    const clone = await item.clone()
-    await clone.update({ data: { quantity: count }})
-    await actor.createOwnedItem(clone.data)
-  }
-}
-
-async function removeItem(actor: Actor5e, item: Item5e, count: number = 1) {
-  const ownedItem = actor.items.get(item.id)
-  if (!ownedItem) {
-    throw notify.error(`failed to find ${item.name} in ${actor.name} to remove`)
-  }
-  const ownedCount = ownedItem.data.data.quantity
-  if (ownedCount === count) {
-    await actor.deleteOwnedItem(item.id)
-  } else if (ownedCount > count) {
-    await ownedItem.update({
-      data: {
-        quantity: ownedCount - count,
-      },
-    })
-  } else {
-    throw notify.error(
-      `attempted to remove ${count} of ${item.name} from ${actor.name} despite them only having ${ownedItem.data.data.quantity}`,
-    )
-  }
-}
+import * as trade from './trade'
 
 /**
  * Attempt to purchase an item from a merchant for a given player
@@ -63,13 +11,12 @@ async function removeItem(actor: Actor5e, item: Item5e, count: number = 1) {
 async function purchase(
   action: Omit<PurchaseAction, 'type'>,
 ): Promise<boolean> {
-  const player = game.actors.get(action.playerId)
-  const merchant = game.actors.get(action.merchantId)
-  const item = merchant?.items?.get(action.itemId)
-  if (!player || !merchant || !item) {
-    notify.error('failed to find entity for purchase')
-    return false
-  }
+  const { player, lootActor: merchant, item } = getParticipants({
+    direction: 'to-player',
+    itemId: action.itemId,
+    playerId: action.playerId,
+    lootActorId: action.merchantId,
+  })
   if (!merchant.data.token.actorLink) {
     notify.error(`remember to link actor data for merchant "${merchant.name}"`)
     return false
@@ -77,9 +24,16 @@ async function purchase(
   const playerCurrency = currency.fromActor(player)
   const itemPrice = currency.fromItem(item)
   if (currency.isMoreThanOrEqualTo(playerCurrency, itemPrice)) {
-    await transferCurrency(player, merchant, itemPrice)
-    await removeItem(merchant, item)
-    await addItem(player, item)
+    await trade.currency({
+      from: player,
+      to: merchant,
+      amount: itemPrice,
+    })
+    await trade.item({
+      from: merchant,
+      to: player,
+      item,
+    })
 
     notify.info(
       `${player.name} purchased ${item.name} from ${
@@ -96,17 +50,12 @@ async function purchase(
 }
 
 async function sell(action: Omit<SellAction, 'type'>): Promise<boolean> {
-  const player = game.actors.get(action.playerId)
-  const merchant = game.actors.get(action.merchantId)
-  const item = player?.items?.get(action.itemId)
-  if (!player || !merchant || !item) {
-    notify.error('failed to find entity for sale')
-    return false
-  }
-  if (!merchant.data.token.actorLink) {
-    notify.error(`remember to link actor data for merchant "${merchant.name}"`)
-    return false
-  }
+  const { player, lootActor: merchant, item } = getParticipants({
+    direction: 'from-player',
+    itemId: action.itemId,
+    playerId: action.playerId,
+    lootActorId: action.merchantId,
+  })
   const merchantCurrency = currency.fromActor(merchant)
   if (item.data.data.quantity > 1) {
     // multi-sale
@@ -125,14 +74,17 @@ async function sell(action: Omit<SellAction, 'type'>): Promise<boolean> {
       }
       const itemPrice = currency.multiply(count, currency.fromItem(item))
       if (currency.isMoreThanOrEqualTo(merchantCurrency, itemPrice)) {
-        // update currency
-        await transferCurrency(merchant, player, itemPrice)
-
-        // remove from actor
-        await removeItem(player, item, count)
-
-        // add to actor
-        await addItem(merchant, item, count)
+        await trade.currency({
+          from: merchant,
+          to: player,
+          amount: itemPrice,
+        })
+        await trade.item({
+          from: player,
+          to: merchant,
+          item,
+          count,
+        })
 
         notify.info(
           `${player.name} sold ${count} of ${item.name} to ${
@@ -151,16 +103,23 @@ async function sell(action: Omit<SellAction, 'type'>): Promise<boolean> {
     const itemPrice = currency.fromItem(item)
     if (currency.isMoreThanOrEqualTo(merchantCurrency, itemPrice)) {
       // single-sale
-      const newItem = await item.clone()
-      await transferCurrency(merchant, player, itemPrice)
-      await player.deleteOwnedItem(item.data._id, {})
-      await merchant.createOwnedItem(newItem.data, {})
+      await trade.currency({
+        from: merchant,
+        to: player,
+        amount: itemPrice,
+      })
+      await trade.item({
+        from: player,
+        to: merchant,
+        item,
+      })
 
       notify.info(
         `${player.name} sold ${item.name} to ${
           merchant.name
         } for ${currency.toString(itemPrice)}`,
       )
+
       return true
     } else {
       notify.info(
---
+++ src/logic/trade.ts
@@ -0,0 +1,72 @@
+import notify from '../util/notify'
+import * as items from './items'
+import * as currencyLogic from './currency'
+
+type Actor5e = game.dnd5e.entities.Actor5e
+type Item5e = game.dnd5e.entities.Item5e
+
+async function removeItem(actor: Actor5e, item: Item5e, count: number = 1) {
+  const ownedItem = actor.items.get(item.id)
+  if (!ownedItem) {
+    throw notify.error(`failed to find ${item.name} in ${actor.name} to remove`)
+  }
+  const ownedCount = ownedItem.data.data.quantity
+  if (ownedCount === count) {
+    await actor.deleteOwnedItem(item.id)
+  } else if (ownedCount > count) {
+    await ownedItem.update({
+      data: {
+        quantity: ownedCount - count,
+      },
+    })
+  } else {
+    throw notify.error(
+      `attempted to remove ${count} of ${item.name} from ${actor.name} despite them only having ${ownedItem.data.data.quantity}`,
+    )
+  }
+}
+
+async function addItem(actor: Actor5e, item: Item5e, count: number = 1) {
+  const ownedItem = actor.items.find((i) => items.compare(i, item))
+  if (ownedItem) {
+    ownedItem.update({
+      data: { quantity: ownedItem.data.data.quantity + count },
+    })
+  } else {
+    const clone = await item.clone()
+    await clone.update({ data: { quantity: count } })
+    await actor.createOwnedItem(clone.data)
+  }
+}
+
+interface TradeItemOpts {
+  from: Actor5e
+  to: Actor5e
+  item: Item5e
+  count?: number
+}
+
+async function item({ from, to, item, count = 1 }: TradeItemOpts) {
+  await removeItem(from, item, count)
+  await addItem(to, item, count)
+}
+
+interface TradeCurrencyOpts {
+  from: Actor5e
+  to: Actor5e
+  amount: Currency
+}
+
+async function currency({ from, to, amount }: TradeCurrencyOpts) {
+  const fromCurrency = from.data.data.currency
+  const toCurrency = to.data.data.currency
+  const { from: freshFromCurrency, to: freshToCurrency } = currencyLogic.transfer({
+    amount,
+    from: fromCurrency,
+    to: toCurrency,
+  })
+  await currencyLogic.updateActor(from, freshFromCurrency)
+  await currencyLogic.updateActor(to, freshToCurrency)
+}
+
+export { item, currency }
